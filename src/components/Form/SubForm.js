762a9d150344980db4c6c989d4d02337e32ab60d
Tristan Berger <tristan.berger6@gmail.com>
2022-01-20 15:57:29 +0100

    subform error msg

--- src/components/Form/SubForm.js
+++ src/components/Form/SubForm.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SubForm.scss';
+import SignForm from './SignForm';
 
 function SubForm({ num }) {
+  const [error, setError] = useState('');
+  const handleSubmit = (e) => {
+    setError('display');
+  };
+
   return (
-    <form className="subForm">
+    <form className="subForm" onSubmit={(e) => handleSubmit(e)}>
       <h3 className="fs-600 fw-400">
         Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous
         abonner ou réactiver votre abonnement
       </h3>
+      <div className={`subForm__error ${error}`}>
+        <SignForm.Error>
+          Ceci est un formulaire purement visuel, cliquez en haut a droite sur
+          le bouton s'identifier pour continuer
+        </SignForm.Error>
+      </div>
       <div className="subForm__block flex">
         <label htmlFor={`email-address${num}`} className="sr-only">
-          Saisir l'addresse e-mail
+          Input uniquement visuelle
         </label>
         <input
           type="text"
           id={`email-address${num}`}
-          placeholder="Adresse e-mail"
+          placeholder=" Adresse e-mail "
           className="fs-400"
         ></input>
         <button type="submit" className="fs-500 btn ">
